ef647d43657eef825d0073e954c6660ae526a476
cfff226 <cazziednb@outlook.com>
2023-12-24 16:40:12 +0000

    Use the with open method to read the output text that the user has previously entered - Loop through each line in the file and output the text to the user - Output options of text editing to the user

--- simple-scribe/simple-scribe.py
+++ simple-scribe/simple-scribe.py
@@ -6,5 +6,14 @@ user_input = str(input("Please enter your text here: "))
 
 # Use the with open method to access create and write to a text file using
 # a relative file path
-with open("output.txt", "w") as f:
+with open("output.txt", "w+") as f:
     f.write(user_input + "\n")
+
+with open("output.txt", "r+") as f:
+    for line in f:
+        print(
+            "\nThis is your text: "
+            + line
+            + "\nHow would you like to edit your text? Choose a number"
+            + "\n\n1. Bold\n2. Italicised\n3. Capitalised\n4. Underlined\n"
+        )
