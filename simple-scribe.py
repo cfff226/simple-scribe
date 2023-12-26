19f851a45f8331c118a235aa11427389c1b575b5
cfff226 <cazziednb@outlook.com>
2023-12-26 12:02:42 +0000

    Concatenate the header, body and footer then assign to a document variable - Output the header, body and footer - Write the document variable to the txt file once the input is given - Read both line of the txt file

--- simple-scribe.py
+++ simple-scribe.py
@@ -5,24 +5,27 @@ while True:
     header = str(input("Please enter your header text here: "))
     body = str(input("Please enter your paragraph text here: "))
     footer = str(input("Please enter your footer text here: "))
-    print(body, body, footer)
+    document = header + "\n" + body + footer
+    print(header, body, footer)
     break
 
 # Use the with open method to access create and write to a text file using
 # a relative file path
 with open("output.txt", "w+") as f:
-    f.write(body + "\n")
+    f.write(document + "\n")
 
 with open("output.txt", "r+") as f:
     for line in f:
         print(
             "\nThis is your text: "
-            + line
+            + line + line
             + "\nHow would you like to edit your text? Choose a number"
             + "\n\n1. Bold\n2. Italicised\n3. Capitalised\n4. Underlined\n"
         )
 
 editing_choice = input("Please input the number of your choice: ")
+
+
 
 
 # Function which applies bold to the user input
@@ -54,20 +57,20 @@ def underlined(body):
     print(body)
     body = f"{body} ## Underlined"
 
+
 # Function which applies a default style to the header
 def default_header(header):
     default_header = default_header.upper()
     default_header = "\033[1m" + default_header + "\033[0m"
     print(default_header)
-    
 
     return default_header
 
+    # Function which calls another function depending on what text edit the user wants to apply
 
-# Function which calls another function depending on what text edit the user wants to apply
 def edit_text():
     if editing_choice == "1":
-        bold(body, )
+        bold(body)
     if editing_choice == "2":
         italicised(body)
     if editing_choice == "3":
