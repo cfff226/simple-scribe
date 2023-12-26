13ed1e0100015da0defcc8bbf4776cd22e3f199a
cfff226 <cazziednb@outlook.com>
2023-12-26 13:32:41 +0000

    Removed functions for italicised, bold, underlined, header - The code has been simplified and now will only require one function to apply various styles - The project will make use of various styling attributes such as colour, text formatting etc

--- simple-scribe.py
+++ simple-scribe.py
@@ -1,17 +1,46 @@
+from __future__ import print_function
+
+# Special END separator
+END = "0e8ed89a-47ba-4cdb-938e-b8af8e084d5c"
+
+# Text attributes
+ALL_OFF = "\033[0m"
+BOLD = "\033[1m"
+UNDERSCORE = "\033[4m"
+BLINK = "\033[5m"
+REVERSE = "\033[7m"
+CONCEALED = "\033[7m"
+
+# Foreground colors
+FG_BLACK = "\033[30m"
+FG_RED = "\033[31m"
+FG_GREEN = "\033[32m"
+FG_YELLOW = "\033[33m"
+FG_BLUE = "\033[34m"
+FG_MAGENTA = "\033[35m"
+FG_CYAN = "\033[36m"
+FG_WHITE = "\033[37m"
+
+# Background colors
+BG_BLACK = "\033[40m"
+BG_RED = "\033[41m"
+BG_GREEN = "\033[42m"
+BG_YELLOW = "\033[43m"
+BG_BLUE = "\033[44m"
+BG_MAGENTA = "\033[45m"
+BG_CYAN = "\033[46m"
+BG_WHITE = "\033[47m"
+
+
 # A program which will enable a user to input and edit text in a document
 # that they create
 
 list_of_strings = []
 
-header = str(input("Please enter your text here: "))
-body = str(input("Please enter your text here: "))
-footer = str(input("Please enter your text here: "))
-user_input = header + body + footer
-list_of_strings.append(header)
-list_of_strings.append(body)
-list_of_strings.append(footer)
+user_input = str(input("Please enter your text here: "))
+list_of_strings.append(user_input)
 print(list_of_strings)
-print(f"{header}                 \n\n{body}                 \n\n{footer}")
+print(user_input)
 
 
 # Use the with open method to access create and write to a text file using
@@ -19,58 +48,17 @@ print(f"{header}                 \n\n{body}                 \n\n{footer}")
 with open("output.txt", "w+") as f:
     f.write(user_input + "\n")
 
-with open("output.txt", "r+") as f:
-    for line in f:
-        print(
-            "\nThis is your text: "
-            + line
-            + "\nHow would you like to edit your text? Choose a number"
-            + "\n\n1. Bold\n2. Italicised\n3. Capitalised\n4. Underlined\n"
-        )
-
-editing_choice = input("Please input the number of your choice: ")
-
 
 # Function which applies bold to the user input
-def bold(user_input):
-    user_input = "\033[1m" + user_input + "\033[0m"
-    print(user_input)
-    user_input = "**" + "\033[1m" + user_input + "\033[0m" + "**"
-
-    return user_input
-
-
-# Function which applies italicised to the user input
-def italicised(user_input):
-    user_input = "\033[3m" + user_input + "\033[3m"
-    print(user_input)
-    user_input = "*" + "\033[1m" + user_input + "\033[0m" + "*"
+def format_text(user_input, list_of_strings):
+    chosen_word = str(
+        input("Please type the text that you would like to apply styles to: ")
+    )
+    if chosen_word in user_input:
+        styles = [BOLD, BG_GREEN, UNDERSCORE]
+        edited_string = "".join(styles) + chosen_word + ALL_OFF
+        user_input = user_input.replace(chosen_word, edited_string)
+        print(user_input)
 
 
-# Function which applies capitalised to the user input
-def capitalised(user_input):
-    user_input = user_input.capitalize()
-    print(user_input)
-    user_input = f"{user_input} ## Capitalised"
-
-
-# Function which applies underlined to the user input
-def underlined(user_input):
-    user_input = "".join([i + "\u0332" for i in user_input])
-    print(user_input)
-    user_input = f"{user_input} ## Underlined"
-
-
-# Function which calls another function depending on what text edit the user wants to apply
-def edit_text():
-    if editing_choice == "1":
-        bold(user_input)
-    if editing_choice == "2":
-        italicised(user_input)
-    if editing_choice == "3":
-        capitalised(user_input)
-    if editing_choice == "4":
-        underlined(user_input)
-
-
-edit_text()
+format_text(user_input, list_of_strings)
