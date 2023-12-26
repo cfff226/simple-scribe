6236a77a9117653d0ade66f194b87fe8710d81f4
cfff226 <cazziednb@outlook.com>
2023-12-26 12:33:46 +0000

    Change whole project back to only requesting 1 instance of user input

--- simple-scribe.py
+++ simple-scribe.py
@@ -1,25 +1,20 @@
 # A program which will enable a user to input and edit text in a document
 # that they create
 
-while True:
-    header = str(input("Please enter your header text here: "))
-    body = str(input("Please enter your paragraph text here: "))
-    footer = str(input("Please enter your footer text here: "))
-    document = header + body + footer
-    print(header, body, footer)
-    break
+user_input = str(input("Please enter your text here: "))
+
 
 # Use the with open method to access create and write to a text file using
 # a relative file path
 with open("output.txt", "w+") as f:
-    f.write(document + "\n")
+    f.write(user_input + "\n")
 
 with open("output.txt", "r+") as f:
     for line in f:
         print(
             "\nThis is your text: "
-            + line + line
-            + "\nHow would you like to edit the main body of your text? Choose a number"
+            + line
+            + "\nHow would you like to edit your text? Choose a number"
             + "\n\n1. Bold\n2. Italicised\n3. Capitalised\n4. Underlined\n"
         )
 
@@ -27,54 +22,45 @@ editing_choice = input("Please input the number of your choice: ")
 
 
 # Function which applies bold to the user input
-def bold(body, footer):
-    body = "\033[1m" + body + "\033[0m"
-    print(body)
-    body = "**" + "\033[1m" + body + "\033[0m" + "**"
+def bold(user_input):
+    user_input = "\033[1m" + user_input + "\033[0m"
+    print(user_input)
+    user_input = "**" + "\033[1m" + user_input + "\033[0m" + "**"
 
-    return body
+    return user_input
 
 
 # Function which applies italicised to the user input
-def italicised(body, footer):
-    body = "\033[3m" + body + "\033[3m"
-    print(body)
-    body = "*" + "\033[1m" + body + "\033[0m" + "*"
+def italicised(user_input):
+    user_input = "\033[3m" + user_input + "\033[3m"
+    print(user_input)
+    user_input = "*" + "\033[1m" + user_input + "\033[0m" + "*"
 
 
 # Function which applies capitalised to the user input
-def capitalised(body, footer):
-    body = body.capitalize()
-    print(body)
-    body = f"{body} ## Capitalised"
+def capitalised(user_input):
+    user_input = user_input.capitalize()
+    print(user_input)
+    user_input = f"{user_input} ## Capitalised"
 
 
 # Function which applies underlined to the user input
-def underlined(body, footer):
-    body = "".join([i + "\u0332" for i in body])
-    print(body)
-    body = f"{body} ## Underlined"
+def underlined(user_input):
+    user_input = "".join([i + "\u0332" for i in user_input])
+    print(user_input)
+    user_input = f"{user_input} ## Underlined"
 
 
-# Function which applies a default style to the header
-def default_header(header):
-    default_header = default_header.upper()
-    default_header = "\033[1m" + default_header + "\033[0m"
-    print(default_header)
-
-    return default_header
-
-    # Function which calls another function depending on what text edit the user wants to apply
-
-def edit_body():
+# Function which calls another function depending on what text edit the user wants to apply
+def edit_text():
     if editing_choice == "1":
-        bold(body, footer)
+        bold(user_input)
     if editing_choice == "2":
-        italicised(body, footer)
+        italicised(user_input)
     if editing_choice == "3":
-        capitalised(body, footer)
+        capitalised(user_input)
     if editing_choice == "4":
-        underlined(body, footer)
+        underlined(user_input)
 
 
-edit_body()
+edit_text()
